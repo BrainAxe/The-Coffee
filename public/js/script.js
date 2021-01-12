7af125d3fd818b505c28f34f92308f600592c3d5
BrainAxe <tanzimrizwan@gmail.com>
2021-01-13 00:33:06 +0600

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+
+burger.addEventListener('click', () => {
+	menu.classList.toggle('hidden');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: {
+	mode: 'layers',
+	content: ['./public/**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+		colors: {
+			primary: '#FF6363',
+			secondary: {
+				100: '#E2E2D5',
+				200: '#888883',
+			}
+		},
+		fontFamily: {
+			body: ['Nunito']
+		}
+	},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
